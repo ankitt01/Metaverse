d7efcb42e1ae2f27b087db387797c0371aec3d1c
ankitt01 <ankitrawat0102@gmail.com>
2021-12-28 19:52:50 +0530

    login page styling

--- components/Login.jsx
+++ components/Login.jsx
@@ -7,8 +7,12 @@ function Login() {
     const {authenticate} = useMoralis()
     return (
         <div className='bg-black relative'>
-            <h1>I am the login screen</h1>
-            <div className='flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4'>
+            
+            <div className='flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4 '>
+                <div className="border-l-8 border-yellow-300 mb-10">
+                    <h3 className='text-2xl text-white font-bold pl-4'>ENTER THE</h3>
+                    <h1 className=' pl-4 font-bold text-6xl text-white'> METAVERSE</h1>    
+                </div>
                 <Image src="https://links.papareact.com/3pi" height={200} width={200} className='object-cover rounded-full'/>
                 <button onClick={authenticate} className='bg-yellow-500 rounded-lg p-5 font-bold animate-pulse'>Login to the METAVERSE</button>
             </div>
